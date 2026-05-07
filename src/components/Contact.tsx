d88f499955ad570fdf0d4b02c990a2388874ec20
Brian Mateos <briansmateos@gmail.com>
2026-05-07 15:50:44 +0000

    ¡GRACIAS POR CONFIAR! (_for element p_)

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -87,7 +87,7 @@ export const Contact = () => {
                     </div>
                     <div>
                       <p className="font-bold text-lg mb-1">Zona de cobertura</p>
-                      <p className="text-muted-foreground leading-relaxed">Río Gallegos, Santa Cruz,<br />Argentina, Z9400</p>
+                      <p className="text-muted-foreground leading-relaxed">Río Gallegos, Santa Cruz, Argentina, Z9400</p>
                     </div>
                   </div>
                   <div className="flex items-start gap-6">
@@ -240,7 +240,7 @@ export const Contact = () => {
               </Form>
               
               <p className="mt-6 text-center text-[10px] text-muted-foreground uppercase tracking-widest font-bold">
-                Gracias por confiar en Pilotos
+                ¡GRACIAS POR CONFIAR!
               </p>
             </div>
           </div>
