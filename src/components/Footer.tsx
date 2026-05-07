4804510b1284f871936b2b9c54c276ee773001df
Brian Mateos <briansmateos@gmail.com>
2026-05-07 16:13:17 +0000

    Make the following changes:
    Find this element:
    
      <li class="text-slate

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -45,7 +45,7 @@ export const Footer = () => {
           <div>
             <h4 className="text-lg font-bold mb-6">Servicios</h4>
             <ul className="space-y-4">
-              <li className="text-slate-400">Licencia de Auto</li>
+              <li className="text-slate-400">Formación para conducir autos</li>
               <li className="text-slate-400">Licencia de Moto</li>
               <li className="text-slate-400">Programas digitales</li>
               <li className="text-slate-400">Asesoría personalizada para la gestión de la licencia</li>
