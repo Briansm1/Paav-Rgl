d983bfdc8112d7756211fbb97b3f47f2ab2f6e05
Brian Mateos <briansmateos@gmail.com>
2026-05-07 23:14:00 +0000

    CAMBIAR LA IMAGEN EN LA SECCION DE SERVICIOS, DE LA mentalidad POR ESTA:

--- src/app/lib/placeholder-images.ts
+++ src/app/lib/placeholder-images.ts
@@ -16,7 +16,7 @@ export const PlaceHolderImages: ImagePlaceholder[] = [
   {
     "id": "service-beginner",
     "description": "Primer plano de volante con manos",
-    "imageUrl": "https://i.imgur.com/HPnM4eC.png",
+    "imageUrl": "https://i.imgur.com/4AK2c4Q.jpeg",
     "imageHint": "car steering"
   },
   {
