00384feee5cd8899157b3a1cfb67c662a774046a
Brian Mateos <briansmateos@gmail.com>
2026-05-09 02:51:22 +0000

    Los detalles para el programa seguridad avanzada (_for element h1_)

--- src/app/planes/page.tsx
+++ src/app/planes/page.tsx
@@ -53,7 +53,7 @@ export default function PlanesPage() {
             Volver al inicio
           </Link>
           <h1 className="text-4xl md:text-6xl font-bold font-headline mb-6 text-foreground text-center">
-            Nuestros detalles para el <span className="text-primary">programa experto</span>
+            Los detalles para el <span className="text-primary">programa seguridad avanzada</span>
           </h1>
         </div>
       </section>
