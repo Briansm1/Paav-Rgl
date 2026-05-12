dd47d37313949ac1677039df2c31774e94807c93
Brian Mateos <briansmateos@gmail.com>
2026-05-12 22:02:34 +0000

    Nuestras sesiones de prácticas dinámicas duran 50 minutos. Es lo ideal p

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -11,7 +11,7 @@ import {
 const faqs = [
   {
     question: "¿Cuánto dura cada clase?",
-    answer: "Nuestras sesiones de prácticas dinámicas duran 50 minutos. Es el tiempo ideal para optimizar tu aprendizaje sin agotarte, permitiéndote fijar los conocimientos de manera efectiva en cada encuentro."
+    answer: "Nuestras sesiones de prácticas dinámicas duran 50 minutos. Es lo ideal para optimizar tu aprendizaje sin agotarte, permitiéndote fijar los conocimientos de manera efectiva en cada encuentro."
   },
   {
     question: "¿Tengo que tener auto para las prácticas?",
