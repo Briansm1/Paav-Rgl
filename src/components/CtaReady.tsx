6a809f802bd8bbf66aaf475c523a68aae9d4b5d2
Brian Mateos <briansmateos@gmail.com>
2026-05-22 15:19:19 +0000

    Quiero empezar (_for element button_)

--- src/components/CtaReady.tsx
+++ src/components/CtaReady.tsx
@@ -26,7 +26,7 @@ export const CtaReady = () => {
           </div>
           <Link href="#planes">
             <Button size="lg" className="h-14 px-10 rounded-full bg-accent hover:bg-accent/90 text-white font-bold text-lg transition-all active:scale-95 shadow-md border-none animate-heartbeat">
-              ¡Si, quiero empezar!
+              Quiero empezar
             </Button>
           </Link>
         </div>
