6a19f69f06d295f8167899fb2db61a8cc6f7018f
Brian Mateos <briansmateos@gmail.com>
2026-05-22 17:01:57 +0000

    sacar los efectos del boton de whatsapp

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -102,17 +102,6 @@ export default {
         'heartbeat': {
           '0%, 100%': { transform: 'scale(1)' },
           '50%': { transform: 'scale(1.08)' },
-        },
-        'cursor-click': {
-          '0%, 100%': { transform: 'translate(15px, 15px) scale(1.1)', opacity: '0' },
-          '20%': { transform: 'translate(0, 0) scale(1.1)', opacity: '1' },
-          '30%': { transform: 'translate(-2px, -2px) scale(0.9)', opacity: '1' },
-          '40%': { transform: 'translate(0, 0) scale(1.1)', opacity: '1' },
-          '80%': { transform: 'translate(0, 0) scale(1.1)', opacity: '1' },
-        },
-        'button-click-sim': {
-          '0%, 25%, 35%, 100%': { transform: 'scale(1)' },
-          '30%': { transform: 'scale(0.93)' },
         }
       },
       animation: {
@@ -120,8 +109,6 @@ export default {
         'accordion-up': 'accordion-up 0.2s ease-out',
         'pulse-soft': 'pulse-soft 4s ease-in-out infinite',
         'heartbeat': 'heartbeat 2s ease-in-out infinite',
-        'cursor-click': 'cursor-click 4s ease-in-out infinite',
-        'button-click-sim': 'button-click-sim 4s ease-in-out infinite',
       },
     },
   },
