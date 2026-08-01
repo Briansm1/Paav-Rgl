387eba97a2f2c49519b2a593f19582003b07946b
Brian Mateos <briansmateos@gmail.com>
2026-08-01 23:07:13 +0000

    podes instalar estos conjuntos de datos de mi pixel en mi web por favor:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,8 +76,33 @@ export default function RootLayout({
             gtag('config', 'G-TXZ2N74HT0');
           `}
         </Script>
+
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '951834753915866');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased">
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=951834753915866&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <FirebaseClientProvider>
           {children}
           <Toaster />
