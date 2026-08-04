9aa18da563314747fc824d6c00b9717199e88c5c
Brian Mateos <briansmateos@gmail.com>
2026-08-04 21:24:03 +0000

    Técnicas de conducción  (_for element li_)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -77,7 +77,7 @@ export const Footer = () => {
           <div className="flex flex-col items-center">
             <h4 className="text-lg font-bold mb-6">Servicios</h4>
             <ul className="space-y-4">
-              <li className="text-slate-400">Formación para conducir vehículos</li>
+              <li className="text-slate-400">Técnicas de conducción</li>
               <li className="text-slate-400">Programas digitales</li>
               <li className="text-slate-400">Asesoría personalizada para gestionar la licencia de conducir</li>
               <li className="text-slate-400">Alquiler del auto para rendir el examen práctico</li>
