ee73d1c0207a94477d85961613705c3d433d785e
Brian Mateos <briansmateos@gmail.com>
2026-08-04 20:17:33 +0000

    Historias de quienes confiaron en nosotros (_for element h3_)

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -93,7 +93,7 @@ export const Testimonials = () => {
             EXPERIENCIAS REALES
           </span>
           <h3 className="text-4xl md:text-5xl font-bold font-headline text-foreground tracking-tight text-center">
-            Historias reales de quienes confiaron en <span className="text-primary italic">nosotros</span>
+            Historias de quienes confiaron en <span className="text-primary italic">nosotros</span>
           </h3>
         </div>
 
