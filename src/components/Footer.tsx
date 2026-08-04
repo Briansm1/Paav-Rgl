0e990bae14f2481ee3d9bd7c6c32ebbd99466955
Brian Mateos <briansmateos@gmail.com>
2026-08-04 21:21:12 +0000

    Formación para conducir vehículos (_for element li_)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -77,7 +77,7 @@ export const Footer = () => {
           <div className="flex flex-col items-center">
             <h4 className="text-lg font-bold mb-6">Servicios</h4>
             <ul className="space-y-4">
-              <li className="text-slate-400">Formación para conducir autos</li>
+              <li className="text-slate-400">Formación para conducir vehículos</li>
               <li className="text-slate-400">Programas digitales</li>
               <li className="text-slate-400">Asesoría personalizada para gestionar tu licencia de conducir</li>
               <li className="text-slate-400">Alquiler del auto para rendir el examen práctico</li>
