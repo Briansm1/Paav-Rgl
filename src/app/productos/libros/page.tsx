bf603bff56b03db8c7f83375582e6a40973c9b7d
Brian Mateos <briansmateos@gmail.com>
2026-08-05 16:16:23 +0000

    digitales en color azul  (_for element h1_)

--- src/app/productos/libros/page.tsx
+++ src/app/productos/libros/page.tsx
@@ -135,7 +135,7 @@ export default function LibrosDigitalesPage() {
           </Link>
           <div className="flex flex-col gap-3">
             <h1 className="text-h1 font-bold font-headline mb-0 text-white text-center">
-              Recursos digitales
+              Recursos <span className="text-primary">digitales</span>
             </h1>
             <p className="text-lg md:text-2xl font-bold text-slate-400 uppercase tracking-[0.2em] text-center max-w-4xl mx-auto">
               Material práctico para reforzar conocimientos al volante
